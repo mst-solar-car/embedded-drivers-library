eb37d4394670f0af6a118efee014fb6c9b66a897
Rouse, Michael W. (S&amp;t-Student) <mwrb7d@mst.edu>
2018-02-27 08:03:44 -0600

    Added depreciated function error message helpers.

--- solarcar.h
+++ solarcar.h
@@ -10,8 +10,30 @@
 #define __SOLARCAR_H__
 #define __LIBRARY_COMPATABILITY_MODE__
 
-#warning You are using 'solarcar.h', this is meant as a temporary compatabile solution for the newer driver library. Please switch to using 'library.h' after switching to new function calls and reading documentation.
+#warning You are using 'solarcar.h', this file no longer exists. Please switch to using 'library.h' after switching to new function calls and reading documentation.
 
 #include "library.h"
+
+/**
+ * Depreciated functions
+ */
+#define USE_NEW(msg) __attribute__((error("Change to " STR(msg))))
+
+// Pin Control
+void USE_NEW("pin_set_mode(pin, mode)")   pinSetMode(io_pin pin, pin_mode mode);
+void USE_NEW("pin_set_level(pin, level)") setPinLevel(io_pin pin, pin_level level);
+void USE_NEW("pin_read(pin)")             readPin(io_pin pin);
+void USE_NEW("pin_set_mode(pin, Input)")  inputPin(io_pin pin);
+void USE_NEW("pin_set_mode(pin, Output")  outputPin(io_pin pin);
+void USE_NEW("pin_set_level(pin, High)")  setPinHigh(io_pin pin);
+void USE_NEW("pin_set_level(pin, Low)")   setPinLow(io_pin pin);
+void USE_NEW("pin_toggle(pin)")           togglePin(io_pin pin);
+void USE_NEW("pin_read(pin) != Low")      isPinHigh(io_pin pin);
+void USE_NEW("pin_read(pin) == Low")      isPinLow(io_pin pin);
+
+// Interrupts
+void USE_NEW("interrupt_attach(pin, func)") attachInterrupt(io_pin pin, void(*func)(void));
+
+
 
 #endif
